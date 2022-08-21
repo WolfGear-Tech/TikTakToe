d7c2a540c8ed0bc8ecc69bd6b292e584dcf5e97c
Matheus Wintruff <mathwintruffac@gmail.com>
2022-08-21 04:51:50 +0100

    Improved Client

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-from socket import socket, gethostbyname, gethostname, AF_INET, SOCK_STREAM
+from socket import *
+#from socket import socket, gethostbyname, gethostname, AF_INET, SOCK_STREAM
 from threading import Thread
 
-SERVER_ADDR = (gethostbyname(gethostname()), 8090)
-ENCODE_FORMAT = "utf-8"
-DISCONECT_MESSAGE = "!DISCONNECT"
-HANDLE_CONNECTION = True
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect(SERVER_ADDR)
+class ClientSocketHandler():
 
-def Send(message):
-    print(f"[SEND] - Sending the message: {message} to the server")
-    clientSocket.send(message.encode(ENCODE_FORMAT))
+    def __init__(self):
+        self.SERVER_ADDR = (gethostbyname(gethostname()), 8090)
+        self.ENCODE_FORMAT = "utf-8"
+        self.DISCONECT_MESSAGE = "!DISCONNECT"
+        self.HANDLE_CONNECTION = True
+        self.clientSocket = socket(AF_INET, SOCK_STREAM)
+        self.clientSocket.connect(self.SERVER_ADDR)
 
-def ResponseListenerHandler():
-    while HANDLE_CONNECTION:
-        recivedMessage = clientSocket.recv(2048).decode(ENCODE_FORMAT)
-        if recivedMessage != DISCONECT_MESSAGE:
-            print(f"[MESSAGE RECIVED] - New Message reciver from client the new message is: {recivedMessage}")        
-        else:
-            print(f"[DISCONNECT] - Closing the connection thread !!")
-            break
-    clientSocket.close()
-    exit()
+    def SendMessage(self, message):
+        print(f"[SEND] - Sending the message: {message} to the server")
+        self.clientSocket.send(message.encode(self.ENCODE_FORMAT))
+        if message == self.DISCONECT_MESSAGE:
+            self.HANDLE_CONNECTION = False
 
-Thread(target=ResponseListenerHandler, daemon=True).start()
+    def ResponseListener(self):
+        while self.HANDLE_CONNECTION:
+            recivedMessage = self.clientSocket.recv(2048).decode(self.ENCODE_FORMAT)
+            if recivedMessage != self.DISCONECT_MESSAGE:
+                print(f"[MESSAGE RECIVED] - New Message reciver from client the new message is: {recivedMessage}")        
+            else:
+                self.HANDLE_CONNECTION = False
+                print(f"[DISCONNECT] - Closing the connection thread !!")
+                break
+        self.clientSocket.close()
+        exit()
 
-while True:
-    message = input("Enter your message: ")
-    Send(message)
+    def StartSocket(self):
+        Thread(target=self.ResponseListener, daemon=True).start()
+        while self.HANDLE_CONNECTION:
+            message = input("Enter your message: ")
+            self.SendMessage(message)
+
+conn = ClientSocketHandler()
+conn.StartSocket()
